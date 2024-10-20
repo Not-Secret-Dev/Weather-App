1d3955b34cdea65ff3cf8aca00576a9bdeb8c8b3
Not-Secret-Dev <ayaanmumtaz8@gmail.com>
2024-10-20 19:48:03 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Weather from "./Components/Weather";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <Weather />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/Weather.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+const Weather = () => {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const API_KEY = "1cf97b79ac70652f79c6114022abd440";
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      if (!response.ok) throw new Error("City not found");
+
+      const data = await response.json();
+      setWeatherData(data);
+      setError(null);
+    } catch (err) {
+      setWeatherData(null);
+      setError(err.message);
+    }
+  };
+
+  const handleSearch = () => {
+    if (city != "") {
+      fetchWeatherData();
+    }
+  };
+
+  return (
+    <>
+      <div className="weather-container">
+        <h1>Weather App</h1>
+        <div className="inputs">
+          <input
+            type="text"
+            placeholder="Enter city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+      </div>
+      {error && <p>{error}</p>}
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}</h2>
+          <p>{weatherData.weather[0].description}</p>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Weather;
